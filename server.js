2728e9a5267289064d3a06a8d6b1bcf902528ee4
yaneza@email.arizona.edu <yaneza@email.arizona.edu>
2026-05-24 22:48:34 -0700

    cors test

--- server.js
+++ server.js
@@ -1,42 +1,13 @@
-// TEST A
-// console.log("LINE 1");
-
-// setTimeout(() => {
-//     console.log("LINE 2");
-// }, 0);
-
-// TEST B
-// import express from "express";
-
-// console.log("AFTER IMPORT");
-
-// const app = express();
-
-// app.listen(8000, () => {
-//   console.log("SERVER STARTED");
-// });
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 import { google } from "googleapis";
+
+dotenv.config();
 
 // console.log("A - imports loaded");
 let sheets;
-
-// function getSheets() {
-//     if (!sheets) {
-//         const auth = new google.auth.GoogleAuth({
-//             keyFile: "credentials.json",
-//             scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-//         });
-
-//         sheets = google.sheets({ version: "v4", auth });
-//     }
-
-//     return sheets;
-// }
 
 function getSheets() {
     if (!sheets) {
@@ -57,32 +28,24 @@ function getSheets() {
     return sheets;
 }
 
-dotenv.config();
-
 // console.log("B - before sheets setup");
 
 const app = express();
 
 app.use(cors({
-  origin: (origin, callback) => {
-    const allowed = [
+    origin: [
       "https://madeforurl.com",
       "https://www.madeforurl.com",
       "https://madeforurl.vercel.app",
       "https://madeforurl-78y5qlb2d-yawneazy-7504s-projects.vercel.app"
-    ];
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    credentials: true
+  }));
 
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
-app.options("*", cors()); 
 app.use(express.json());
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 app.get("/test", (req, res) => {
     res.json({ ok: true });
@@ -131,3 +94,20 @@ app.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
 
+// TEST A
+// console.log("LINE 1");
+
+// setTimeout(() => {
+//     console.log("LINE 2");
+// }, 0);
+
+// TEST B
+// import express from "express";
+
+// console.log("AFTER IMPORT");
+
+// const app = express();
+
+// app.listen(8000, () => {
+//   console.log("SERVER STARTED");
+// });
